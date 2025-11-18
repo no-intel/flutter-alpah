56d033152c93bccebca5ebada8b158bc403a854e
no-int <tkdduf2122@naver.com>
2025-11-19 02:36:56 +0900

    flutter 1

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SplashScreen()); // ① SplashScreen 위젯을 첫 화면으로 지정
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+          child: Center(
+            child: Text("Splash Screen"),
+          ),
+        ),
+      ),
+    );
+  }
+}
